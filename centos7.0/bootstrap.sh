d389571487f6e7770d0e2854efab92536089dd48
Jason Plurad <pluradj@users.noreply.github.com>
2016-02-01 14:06:33 -0500

    update CentOS 7 box

--- centos7.0/bootstrap.sh
+++ centos7.0/bootstrap.sh
@@ -3,16 +3,18 @@
 cp /vagrant/hosts /etc/hosts
 cp /vagrant/resolv.conf /etc/resolv.conf
 yum install ntp -y
+sed -i 's/centos.pool.ntp.org/us.pool.ntp.org/g' /etc/ntp.conf
 service ntpd start
-service iptables stop
+chkconfig ntpd on
 mkdir -p /root/.ssh; chmod 600 /root/.ssh; cp /home/vagrant/.ssh/authorized_keys /root/.ssh/
-
-#Again, stopping iptables
-/etc/init.d/iptables stop
 
 # Increasing swap space
 sudo dd if=/dev/zero of=/swapfile bs=1024 count=1024k
 sudo mkswap /swapfile
+echo 10 | sudo tee /proc/sys/vm/swappiness
+echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
+sudo chown root:root /swapfile
+sudo chmod 600 /swapfile
 sudo swapon /swapfile
 echo "/swapfile       none    swap    sw      0       0" >> /etc/fstab
 
@@ -20,4 +22,6 @@ sudo cp /vagrant/insecure_private_key /root/ec2-keypair
 sudo chmod 600 /root/ec2-keypair
 
 # Workaround from https://www.digitalocean.com/community/questions/can-t-install-mysql-on-centos-7
-rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+#rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+
+yum install wget curl git unzip zip vim -y
